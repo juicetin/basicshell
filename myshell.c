bcf9e937b9cc4e86b317a4910e838e2d7b3851c9
Justin Ting <justingling@gmail.com>
2015-03-09 14:09:07 +1100

    few commands working

--- myshell.c
+++ myshell.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 void change_dir () {
 
@@ -15,6 +16,7 @@ void dir () {
 
 void environ () {
 
+
 }
 
 void echo () {
@@ -25,7 +27,14 @@ void help () {
 
 }
 
-void pause () {
+//Naming to avoid conflict with stdlib.h library
+void shell_pause () {
+  for(;;) {
+    char c = getchar();
+    c = getchar();
+    if (c == '\n')
+      break;
+  }
 
 }
 
@@ -34,13 +43,25 @@ void quit () {
 }
 
 int main (int argc, char * argv[]) {
+  
   for (;;) {
+    
+    //Get current directory
+    char cwd[1024];
+    getcwd(cwd, sizeof(cwd));
+    printf("%s ~ ", cwd); 
+
+    //Read user input
     char str[100];
     scanf("%s", str);
     
+    //Show cwd if only enter pressed
+    if (str[0] == "\n")
+      continue;
+
     //Pause block
     if (strcmp(str, "pause") == 0)
-
+      shell_pause();
 
     //Quit block
     if (strcmp(str, "quit") == 0)
